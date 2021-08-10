c79deb6c342959ac1eab1cec13b7a59b1017a6d8
saifeddine1992 <saifeddinebouazizi1992@gmail.com>
2021-08-10 14:52:10 +0100

    AddressHistoryBuilder only works on current=true history

--- src/main/scala/com/instagram/AdressHistoryBuilder.scala
+++ src/main/scala/com/instagram/AdressHistoryBuilder.scala
@@ -1,7 +1,7 @@
 package com.instagram
 
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, current_date, datediff, lit}
+import org.apache.spark.sql.functions.{col, lit}
 
 
 object AddressHistoryBuilder {
@@ -9,23 +9,23 @@ object AddressHistoryBuilder {
     val renamedUpdateFields = update.withColumnRenamed("address", "new_address")
       .withColumnRenamed("moved_in", "new_moved_in").select("id", "new_address", "new_moved_in")
 
-    val joinedDataFrames = history.join(renamedUpdateFields, "id")
+    val joinedHistoryAndUpdate = history.where(col("current") === true).join(renamedUpdateFields, "id")
 
-    val case1 = joinedDataFrames.where(col("new_moved_in") > col("moved_in"))
-        val rowsFixed = case1
+    val newRecordAddedOnDifferentAdress = joinedHistoryAndUpdate.where(col("new_moved_in") > col("moved_in"))
+        val rowsFixed = newRecordAddedOnDifferentAdress
           .withColumn("moved_out", col("new_moved_in"))
           .withColumn("current", lit(false))
           .drop("new_address", "new_moved_in")
-        val rowsAdded1 = case1
+        val rowsAdded1 = newRecordAddedOnDifferentAdress
           .withColumn("address", col("new_address"))
           .withColumn("moved_in", col("new_moved_in"))
           .drop("new_address", "new_moved_in")
-        val rowsToBeRemoved = case1.drop("new_address", "new_moved_in")
+        val rowsToBeRemoved = newRecordAddedOnDifferentAdress.drop("new_address", "new_moved_in")
 
 
 
-    val case2 = joinedDataFrames.where(col("new_moved_in")  < col("moved_in"))
-    val rowsAdded = case2
+    val oldRecordAddedOnDifferentAdress = joinedHistoryAndUpdate.where(col("new_moved_in")  < col("moved_in"))
+    val rowsAdded = oldRecordAddedOnDifferentAdress
           .withColumn("moved_out", col("moved_in"))
           .withColumn("current", lit(false))
           .withColumn("moved_in", col("new_moved_in"))
--- src/test/scala/com/instagram/AdressHistoryBuilderSpec.scala
+++ src/test/scala/com/instagram/AdressHistoryBuilderSpec.scala
@@ -25,6 +25,7 @@ class AddressHistoryBuilderSpec  extends AnyFlatSpec with Matchers with GivenWhe
   val pattern = DateTimeFormatter.ofPattern("dd-MM-yyyy")
   val adressHistory = Seq(
     AddressHistory(1, "Sayf", "Bouazizi", "Kasserine", LocalDate.parse("21-11-1992", pattern), null, true),
+    AddressHistory(1, "Sayf", "Bouazizi", "Kasserine", LocalDate.parse("21-11-1960", pattern), LocalDate.parse("21-11-1961", pattern), false),
     AddressHistory(2, "jasser", "rtibi", "amsterdam", LocalDate.parse("21-11-2020", pattern), null, true)).toDF()
 
   val historyUpdate = Seq(
@@ -35,6 +36,7 @@ class AddressHistoryBuilderSpec  extends AnyFlatSpec with Matchers with GivenWhe
   val expectedResult = Seq(
     AddressHistory(1, "Sayf", "Bouazizi", "Sousse", LocalDate.parse("06-06-2017", pattern), null , true),
     AddressHistory(1,"Sayf", "Bouazizi", "Kasserine", LocalDate.parse("21-11-1992", pattern), LocalDate.parse("06-06-2017", pattern), false),
+    AddressHistory(1, "Sayf", "Bouazizi", "Kasserine", LocalDate.parse("21-11-1960", pattern), LocalDate.parse("21-11-1961", pattern), false),
     AddressHistory(2, "jasser", "rtibi", "amsterdam", LocalDate.parse("21-11-2020", pattern), null, true),
     AddressHistory(2, "jasser", "rtibi", "USA", LocalDate.parse("21-11-1992", pattern), LocalDate.parse("21-11-2020", pattern), false)
   ).toDF()
