24fa0e01e4ac33610a2c11541577fb775b7d7667
saifeddine1992 <saifeddinebouazizi1992@gmail.com>
2021-08-13 11:59:28 +0100

    AddressHistoryBuilder all cases tested

--- src/main/scala/com/instagram/AdressHistoryBuilder.scala
+++ src/main/scala/com/instagram/AdressHistoryBuilder.scala
@@ -6,10 +6,11 @@ import org.apache.spark.sql.functions.{col, lit}
 
 object AddressHistoryBuilder {
   def addressHistoryBuilder(history: DataFrame, update: DataFrame): DataFrame = {
+
     val renamedUpdateFields = update.withColumnRenamed("address", "new_address")
       .withColumnRenamed("moved_in", "new_moved_in").select("id", "new_address", "new_moved_in")
-    val historyThatMatters = history.where(col("current") === true)
 
+    val historyThatMatters = history.where(col("current") === true)
     val historyWithHousesLeft = history.where(col("current") === false)
       .withColumnRenamed("moved_out" , "old_moved_out")
       .withColumnRenamed("moved_in" , "old_moved_in")
@@ -32,6 +33,7 @@ object AddressHistoryBuilder {
     val rowsFixed = recordAddedOnDifferentAdressWithDateCondition1.withColumn("address", col("new_address"))
       .withColumn("moved_in", col("new_moved_in"))
       .drop("new_address", "new_moved_in")
+
     val rowsToBeRemoved = recordAddedOnDifferentAdressWithDateCondition1.drop("new_address", "new_moved_in")
 
 
@@ -45,47 +47,47 @@ object AddressHistoryBuilder {
 
 
     val recordAddedOnSameAdress = joinedHistoryAndUpdate.where(col("new_address") === col("address"))
+
     val recordAddedOnSameAdressWithDateCondition = recordAddedOnSameAdress.where(col("new_moved_in") < col("moved_in"))
 
     val rowsAdded2 = recordAddedOnSameAdressWithDateCondition
       .withColumn("moved_in", col("new_moved_in"))
       .drop("new_address", "new_moved_in")
+
     val rowsToBeRemoved1 = recordAddedOnSameAdressWithDateCondition.drop("new_address", "new_moved_in")
 
 
     val newRecordsToBeAdded = update.join(historyThatMatters, historyThatMatters("id") === update("id"), "leftanti")
-    val addedRecord = newRecordsToBeAdded.withColumn("moved_out", lit(null)).withColumn("current", lit(true))
+    val rowsAdded3 = newRecordsToBeAdded.withColumn("moved_out", lit(null)).withColumn("current", lit(true))
 
 
 
     val joinedAdditionWithLeftHouses = rowsAdded.join(historyWithHousesLeft , "id")
     val interval1 = joinedAdditionWithLeftHouses.where(col("moved_in") > col("old_moved_in"))
     val case1 = interval1.where(col("moved_in") < col("old_moved_out"))
-    val rowAddedd1 = case1.withColumn("moved_out" , col("old_moved_out"))
+    val rowAdded4 = case1.withColumn("moved_out" , col("old_moved_out"))
       .drop("old_address" , "old_moved_in" , "old_moved_out")
-    val rowsAddedd2 = case1
+    val rowsAdded5 = case1
       .withColumn("moved_out" , col("moved_in"))
       .withColumn("moved_in" , col("old_moved_in"))
       .withColumn("address" , col("old_address")).drop("old_address" , "old_moved_in" , "old_moved_out")
-    val rowtoberemovedd = case1.withColumn("moved_in" , col("old_moved_in"))
+    val rowsToBeRemoved2 = case1.withColumn("moved_in" , col("old_moved_in"))
       .withColumn("moved_out" , col("old_moved_out"))
       .withColumn("address" , col("old_address")).drop("old_address" , "old_moved_in" , "old_moved_out")
-    val rowstoberemovedd1 = case1.drop("old_address" , "old_moved_in" , "old_moved_out")
+    val rowsToBeRemoved3 = case1.drop("old_address" , "old_moved_in" , "old_moved_out")
 
 
 
 
     val interval2 = joinedAdditionWithLeftHouses.where(col("moved_in") < col("old_moved_in"))
-    val rowAddedAgain = interval2.withColumn("moved_out" , col("old_moved_in"))
+    val rowsAdded6 = interval2.withColumn("moved_out" , col("old_moved_in"))
       .drop("old_address" , "old_moved_in" , "old_moved_out").sort("moved_out").dropDuplicates("moved_in")
-    val rowsToBeOmmited = interval2.drop("old_address" , "old_moved_in" , "old_moved_out")
+    val rowsToBeRemoved4 = interval2.drop("old_address" , "old_moved_in" , "old_moved_out")
 
 
 
-
-
-    val additions = rowsAdded2.union(rowsAdded).union(rowsAdded1).union(rowsFixed).union(addedRecord).union(rowAddedd1).union(rowsAddedd2).union(rowAddedAgain)
-    val toBeRemoved = rowsToBeRemoved.union(rowsToBeRemoved1).union(rowtoberemovedd).union(rowstoberemovedd1).union(rowsToBeOmmited)
+    val additions = rowsAdded2.union(rowsAdded).union(rowsAdded1).union(rowsFixed).union(rowsAdded3).union(rowAdded4).union(rowsAdded5).union(rowsAdded6)
+    val toBeRemoved = rowsToBeRemoved.union(rowsToBeRemoved1).union(rowsToBeRemoved2).union(rowsToBeRemoved3).union(rowsToBeRemoved4)
     history.union(additions).except(toBeRemoved)
   }
 }
