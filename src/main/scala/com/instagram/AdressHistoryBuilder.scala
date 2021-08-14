ede183188ed7d830ceb8f45d8d6ff69091cd3b8a
saifeddine1992 <saifeddinebouazizi1992@gmail.com>
2021-08-14 12:53:26 +0100

    AddressHistoryBuilder all cases tested

--- src/main/scala/com/instagram/AdressHistoryBuilder.scala
+++ src/main/scala/com/instagram/AdressHistoryBuilder.scala
@@ -14,18 +14,18 @@ object AddressHistoryBuilder {
     val joinedHistoryAndUpdate = getTouchedHistoryForNewArrivingData(history, update)
     val newArrivingDataOnDifferentAddress = getNewArrivingDataWithDifferentAddress(joinedHistoryAndUpdate)
 
-    val rowsAdded1 = newArrivingDataOnDifferentAddress
+    val rowsAddedOnDifferentAddress = newArrivingDataOnDifferentAddress
       .withColumn("moved_out", col("new_moved_in"))
       .withColumn("current", lit(false)).drop("new_address", "new_moved_in")
-    val rowsFixed = newArrivingDataOnDifferentAddress.withColumn("address", col("new_address"))
+    val newCurrentRowsAdded = newArrivingDataOnDifferentAddress.withColumn("address", col("new_address"))
       .withColumn("moved_in", col("new_moved_in"))
       .drop("new_address", "new_moved_in")
-    val rowsToBeRemoved = newArrivingDataOnDifferentAddress.drop("new_address", "new_moved_in")
+    val OldCurrentRowsToBeRemoved = newArrivingDataOnDifferentAddress.drop("new_address", "new_moved_in")
 
 
 
     val lateArrivingDataOnDifferentAddress = getLateArrivingDataWithDifferentAddress(joinedHistoryAndUpdate)
-    val rowsAdded = lateArrivingDataOnDifferentAddress
+    val PotentiallyAddedRows = lateArrivingDataOnDifferentAddress
       .withColumn("moved_out", col("moved_in"))
       .withColumn("current", lit(false))
       .withColumn("moved_in", col("new_moved_in"))
@@ -35,53 +35,64 @@ object AddressHistoryBuilder {
 
 
     val lateArrivingDataOnSameAddress = getLateArrivingDataWithSameAddress(joinedHistoryAndUpdate)
-    val rowsAdded2 = lateArrivingDataOnSameAddress
+    val rowsAddedOnSameAddress = lateArrivingDataOnSameAddress
       .withColumn("moved_in", col("new_moved_in"))
       .drop("new_address", "new_moved_in")
-    val rowsToBeRemoved1 = lateArrivingDataOnSameAddress.drop("new_address", "new_moved_in")
+    val rowsRemovedOnSameAddress = lateArrivingDataOnSameAddress.drop("new_address", "new_moved_in")
 
 
 
     val newPeopleOnHistory = getNewPeopleOnHistory(update, joinedHistoryAndUpdate)
-    val rowsAdded3 = newPeopleOnHistory
+    val newPeopleRows = newPeopleOnHistory
       .withColumn("moved_out", lit(null))
       .withColumn("current", lit(true))
 
 
 
-    val joinedAdditionWithLeftHouses = getTouchedHistoryForLateArrivingData(history, rowsAdded)
+    val joinedAdditionWithLeftHouses = getTouchedHistoryForLateArrivingData(history, PotentiallyAddedRows)
 
     val updateThatOverlapsWithLateHistory = getTouchedHistoryOfRecordsOverlappingWithUpdates(joinedAdditionWithLeftHouses)
 
-    val rowAdded4 = updateThatOverlapsWithLateHistory
+    val rowsOfUpdateAdded = updateThatOverlapsWithLateHistory
       .withColumn("moved_out" , col("old_moved_out"))
       .drop("old_address" , "old_moved_in" , "old_moved_out")
-    val rowsAdded5 = updateThatOverlapsWithLateHistory
+    val rowsOfHistoryCorrected = updateThatOverlapsWithLateHistory
       .withColumn("moved_out" , col("moved_in"))
       .withColumn("moved_in" , col("old_moved_in"))
       .withColumn("address" , col("old_address"))
       .drop("old_address" , "old_moved_in" , "old_moved_out")
-    val rowsToBeRemoved2 = updateThatOverlapsWithLateHistory
+    val OldRowsOfHistoryRemoved = updateThatOverlapsWithLateHistory
       .withColumn("moved_in" , col("old_moved_in"))
       .withColumn("moved_out" , col("old_moved_out"))
       .withColumn("address" , col("old_address"))
       .drop("old_address" , "old_moved_in" , "old_moved_out")
-    val rowsToBeRemoved3 = updateThatOverlapsWithLateHistory
+    val PotentiallyAddedRowsWhenOverlapping = updateThatOverlapsWithLateHistory
       .drop("old_address" , "old_moved_in" , "old_moved_out")
 
 
 val updatesOlderThanAllOfHistory = getTouchedHistoryWhenUpdatesAreOlderThanHistory(joinedAdditionWithLeftHouses)
-    val rowsAdded6 = updatesOlderThanAllOfHistory
+    val OldestRowsAdded = updatesOlderThanAllOfHistory
       .withColumn("moved_out" , col("old_moved_in"))
       .drop("old_address" , "old_moved_in" , "old_moved_out")
       .sort("moved_out")
       .dropDuplicates("moved_in")
-    val rowsToBeRemoved4 = updatesOlderThanAllOfHistory.drop("old_address" , "old_moved_in" , "old_moved_out")
+    val PotentiallyAddedRowsWhenOldestRecord = updatesOlderThanAllOfHistory
+      .drop("old_address" , "old_moved_in" , "old_moved_out")
 
 
-
-    val additions = rowsAdded2.union(rowsAdded).union(rowsAdded1).union(rowsFixed).union(rowsAdded3).union(rowAdded4).union(rowsAdded5).union(rowsAdded6)
-    val toBeRemoved = rowsToBeRemoved.union(rowsToBeRemoved1).union(rowsToBeRemoved2).union(rowsToBeRemoved3).union(rowsToBeRemoved4)
+    val additions = rowsAddedOnDifferentAddress
+      .union(newCurrentRowsAdded)
+      .union(PotentiallyAddedRows)
+      .union(rowsAddedOnSameAddress)
+      .union(newPeopleRows)
+      .union(rowsOfUpdateAdded)
+      .union(rowsOfHistoryCorrected)
+      .union(OldestRowsAdded)
+    val toBeRemoved = OldCurrentRowsToBeRemoved
+      .union(rowsRemovedOnSameAddress)
+      .union(OldRowsOfHistoryRemoved)
+      .union(PotentiallyAddedRowsWhenOverlapping)
+      .union(PotentiallyAddedRowsWhenOldestRecord)
     history.union(additions).except(toBeRemoved)
   }
 }
